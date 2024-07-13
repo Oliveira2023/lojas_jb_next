6b55361c61b137f31c0138cbe2593392d3694084
Luciano <luciano.oliveira96@yahoo.com.br>
2024-07-12 23:54:29 -0300

    expose apikey directly

--- components/maps.tsx
+++ components/maps.tsx
@@ -2,7 +2,7 @@ import { GoogleMapsEmbed } from "@next/third-parties/google"
 
 export default function Maps({local, width}: any) {
     // const apiKey = process.env.NEXT_PUBLIC_MapsApi;
-    const apiKey = process.env.MAPS_API;
+    const apiKey = 'AIzaSyD310CkG0wT9mqQgjapyhsnBDA4QycESjM';
     // const apiKey = process.env.MapsApi ?? 'AIzaSyD310CkG0wT9mqQgjapyhsnBDA4QycESjM'
     // o problema aqui é que este componente é client side, pra poder usar a chave de api corretamente tenho que tornar server side.
     return (
