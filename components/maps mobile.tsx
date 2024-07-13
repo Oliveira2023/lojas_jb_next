c3ca00e779eb68e6c2e34673cb1c4a59cd7cd9ad
Luciano <luciano.oliveira96@yahoo.com.br>
2024-07-12 23:50:57 -0300

    try captalize apikey

--- components/maps mobile.tsx
+++ components/maps mobile.tsx
@@ -2,12 +2,13 @@
 import { GoogleMapsEmbed } from "@next/third-parties/google"
 
 export default function Maps({local}: any) {
-    const apiKey = process.env.MapsApi ?? 'AIzaSyD310CkG0wT9mqQgjapyhsnBDA4QycESjM'
+    const apiKey = process.env.MAPS_API;
+    // const apiKey = process.env.MapsApi ?? 'AIzaSyD310CkG0wT9mqQgjapyhsnBDA4QycESjM'
     // o problema aqui é que este componente é client side, pra poder usar a chave de api corretamente tenho que tornar server side.
 
     return (
         <GoogleMapsEmbed
-        apiKey={apiKey}
+        apiKey={apiKey ?? 'defaultApiKey'}
         height={300}
         width={300}
         mode="place"
