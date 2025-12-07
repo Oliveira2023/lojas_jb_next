8d6a3e37feb2ddb9c598c1edcb0b1c989ecaa0f6
Luciano Oliveira <luciano.oliveira96@yahoo.com.br>
2025-12-07 18:52:05 -0300

    update next.config.ts to mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-import './src/app/config/envConfig';
 const nextConfig = {
-    
-
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+      },
 };
 
 export default nextConfig;
